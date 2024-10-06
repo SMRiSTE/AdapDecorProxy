4f03ec210c68fcaf61272069d926d8f3b53ff16e
SMRist <s.m.r.00800ist@gmail.com>
2024-10-07 00:01:42 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+#include <string>
+#include <iostream>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+class Paragraph : public DecoratedText {
+public:
+    explicit Paragraph(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public DecoratedText {
+public:
+    explicit Reversed(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::string rev_data = " ";
+        for (int i = data.length(); i > -1; --i) {
+            rev_data += data[i];
+        }
+        text_->render(rev_data);
+    }
+};
+
+class Link : public DecoratedText {
+public:
+    explicit Link(Text* text) : DecoratedText(text) {}
+    void render(const std::string link, const std::string& data) const {
+        std::cout << "<a href=" << link << ">";
+        text_->render(data);
+        std::cout << "</a>";
+    }
+};
+
+int main()
+{
+    auto text_block = new Link(new Reversed(new Text()));
+    text_block->render("netology.ru", "Hello World!");
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,76 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+#include <vector>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "value";
+    }
+};
+
+class CacheProxyDB : VeryHeavyDatabase {
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (cache_.find(key) == cache_.end()) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class TestDB : VeryHeavyDatabase {
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
+
+class OneShotDB : VeryHeavyDatabase {
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots) : real_db_(real_object), shots_(shots) {}
+    std::string GetData(std::string key) noexcept {
+        if (map_.find(key) == map_.end()) {
+            map_[key] = real_db_->GetData(key);
+            vec.push_back(1);
+            return map_.at(key);
+        }
+        else {
+            if (vec.size() == shots_) {
+                return "error";
+            }
+            else {
+                vec.push_back(1);
+                return map_.at(key);
+            }
+        }
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+    size_t shots_;
+    std::map<std::string, std::string> map_;
+    std::vector<int> vec = {};
+};
+
+int main() {
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+
+    return 0;
+}
